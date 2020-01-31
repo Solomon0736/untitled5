c41556dd189e6d9742b53038d1fc7ae4afbbb001
solomontikabo.musie <Seli1230>
2020-01-31 11:19:50 +0100

    Initial commit

---
+++ src/komplex.java
@@ -0,0 +1,11 @@
+public class komplex {
+    private double a;
+    private double b;
+    }
+    public static void main(String[] arg) {
+     public komplex( double a){
+            this.a = a;
+        }
+    }
+}
+
