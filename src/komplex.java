c26a34ff52ea84b51395d88e736a3d69a8f26c45
solomontikabo.musie <Seli1230>
2020-02-05 15:58:55 +0100

    uyu

--- src/komplex.java
+++ src/komplex.java
@@ -1,11 +1,14 @@
-public class komplex {
+public class    komplex {
     private double a;
     private double b;
-    }
     public static void main(String[] arg) {
-     public komplex( double a){
+
+        public komplex(double a){
             this.a = a;
+
         }
     }
 }
+
+
 
